93491497bb85951c3b51066bba027f264a859b27
Tom Liang <tomliangg@gmail.com>
2022-01-28 18:28:52 -0800

    init project with create-ink-app

---
+++ source/cli.tsx
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './ui';
+
+const cli = meow(`
+	Usage
+	  $ stock-helper
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ stock-helper --name=Jane
+	  Hello, Jane
+`, {
+	flags: {
+		name: {
+			type: 'string'
+		}
+	}
+});
+
+render(<App name={cli.flags.name}/>);
