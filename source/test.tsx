b52065175ef32b8a4bb0b76614c0833b3398d07e
Tom Liang <tomliangg@gmail.com>
2022-01-30 10:18:03 -0800

    update test

--- source/test.tsx
+++ source/test.tsx
@@ -4,14 +4,7 @@ import test from 'ava';
 import {render} from 'ink-testing-library';
 import App from './ui';
 
-test('greet unknown user', t => {
-	const {lastFrame} = render(<App/>);
-
-	t.is(lastFrame(), chalk`Hello, {green Stranger}`);
-});
-
-test('greet user with a name', t => {
-	const {lastFrame} = render(<App name="Jane"/>);
-
-	t.is(lastFrame(), chalk`Hello, {green Jane}`);
+test('should render correctly', t => {
+	const {lastFrame} = render(<App apiKey="123jix" symbols={['FB', 'GOOG']}/>);
+	t.is(lastFrame(), chalk`{cyan Loading...}`);
 });
